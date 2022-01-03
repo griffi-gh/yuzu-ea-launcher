91f863d248e0d7c6d0ffebe7f4a66b244c946234
griffi-gh <prasol258@gmail.com>
2022-01-03 19:55:46 +0200

    YuzuEALauncher

---
+++ launcher.bat
@@ -0,0 +1,65 @@
+@echo off
+setlocal
+
+where gh>nul 2>nul
+if %ERRORLEVEL% NEQ 0 goto :gherr
+
+:start
+
+cd /d %~dp0
+
+echo E - Exit
+echo U - Update/Install
+echo [empty] - Launch
+
+set /P c=? 
+if /I "%c%" EQU "U" goto :update
+if /I "%c%" EQU "E" goto :end
+goto :launch
+
+:launch
+
+cd yuzu-early-access
+start yuzu.exe
+
+goto :end
+exit
+
+:update
+
+echo|set /p="Cleaning up..."
+IF exist yuzu-early-access ( rd /s /q yuzu-early-access )
+echo [Done]
+
+echo|set /p="Downloading the latest release from GitHub..."
+gh release download --repo Kryptuq/Yuzu-Early-Access-files --pattern ** > nul
+echo [Done]
+
+echo|set /p="Cleaning up..."
+dir /b/s *yuzu*.zip > .temp
+set /p path=<.temp
+del .temp
+ren "%path%" latest.zip
+echo [Done]
+
+echo|set /p="Extracting..."
+7za x latest.zip -o"yuzu-early-access" > nul
+echo [Done]
+
+echo|set /p="Cleaning up..."
+del latest.zip
+echo [Done]
+
+echo.
+echo [OK] Installed successfully!
+echo.
+
+goto :start
+
+:gherr
+echo [ERROR] Please install GitHub CLI and authenticate
+echo https://cli.github.com/
+goto :end
+
+:end
+rem
